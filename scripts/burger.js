207a1d3f7ff6359d703179c24023f1538cfe8f3e
jocooper <jocooper2018@gmail.com>
2024-03-21 11:04:11 +0100

    Ajout du menu burger

---
+++ scripts/burger.js
@@ -0,0 +1,13 @@
+
+function openBurgerMenu()
+{
+    var burgerMenu = document.getElementById("burger_menu");
+    if (burgerMenu.style.display == "none")
+    {
+        burgerMenu.style.display = "block";
+    }
+    else
+    {
+        burgerMenu.style.display = "none";
+    }
+}
