8e6b85d1b3e4d0be54dd0e6d3e74774f379bf3d5
jocooper <jocooper2018@gmail.com>
2025-10-20 14:28:17 +0200

    fix: Adjusted SVG dimensions and attributes for KeyboardArrowDownIcon and KeyboardArrowUpIcon; improved CSS styles for project and select components

--- src/assets/icons/KeyboardArrowUpIcon.tsx
+++ src/assets/icons/KeyboardArrowUpIcon.tsx
@@ -1,12 +1,18 @@
 const KeyboardArrowUpIcon: React.FC = () => (
   <svg
-    xmlns="http://www.w3.org/2000/svg"
-    height="24px"
-    viewBox="0 -960 960 960"
-    width="24px"
+    height="6.5749998"
+    viewBox="0 -960 445.99998 262.99999"
+    width="11.15"
     className="icon"
+    version="1.1"
+    id="svg1"
+    xmlns="http://www.w3.org/2000/svg"
   >
-    <path d="M480-528 324-372q-11 11-28 11t-28-11q-11-11-11-28t11-28l184-184q12-12 28-12t28 12l184 184q11 11 11 28t-11 28q-11 11-28 11t-28-11L480-528Z" />
+    <defs id="defs1" />
+    <path
+      d="M 223,-864 67,-708 q -11,11 -28,11 -17,0 -28,-11 -11,-11 -11,-28 0,-17 11,-28 l 184,-184 q 12,-12 28,-12 16,0 28,12 l 184,184 q 11,11 11,28 0,17 -11,28 -11,11 -28,11 -17,0 -28,-11 z"
+      id="path1"
+    />
   </svg>
 );
 
