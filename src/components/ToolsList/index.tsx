d088f947b7b99cf502c1dce378851e8ea2f94df8
jocooper <jocooper2018@gmail.com>
2026-01-12 16:30:22 +0100

    feat(tools): Add navigation buttons for overflowing tools list and implement icon components

--- src/components/ToolsList/index.tsx
+++ src/components/ToolsList/index.tsx
@@ -16,13 +16,15 @@
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { useContext } from "react";
+import "./index.css";
+import { useContext, useEffect, useRef, useState } from "react";
 import { useLanguage } from "../../contexts/LanguageContext";
 import type Tool from "../../interfaces/Tool";
 import getIconUrlForTheme from "../../utils/getIconUrlForTheme";
 import getTool from "../../utils/getTool";
-import "./index.css";
 import { ThemeContext } from "../../contexts/ThemeContext";
+import KeyboardArrowLeftIcon from "../../assets/icons/KeyboardArrowLeftIcon";
+import KeyboardArrowRightIcon from "../../assets/icons/KeyboardArrowRightIcon";
 
 interface ToolsListProps {
   readonly toolsIds: string[];
@@ -34,29 +36,90 @@ const ToolsList: React.FC<ToolsListProps> = (props: ToolsListProps) => {
   const { lang } = useLanguage();
   const { resolvedTheme } = useContext(ThemeContext);
 
+  const ulRef = useRef<HTMLUListElement>(null);
+
+  const [isOverflowing, setIsOverflowing] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!ulRef.current) {
+      return;
+    }
+    const checkOverflow = () => {
+      if (!ulRef.current) {
+        return;
+      }
+      setIsOverflowing(ulRef.current.scrollWidth > ulRef.current.clientWidth);
+    };
+    const resizeObserver = new ResizeObserver(checkOverflow);
+    resizeObserver.observe(ulRef.current);
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, []);
+
   return (
-    <ul className="tools-list">
-      {props.toolsIds.map((toolId: string) => {
-        if (props.isToolsLoading) {
-          return <div className="loader"></div>;
-        }
-        const tool: Tool | undefined = getTool(props.everyTools, toolId);
-        if (!tool) {
-          throw new Error(`tool ${toolId} not found`);
-        }
-        return (
-          <li key={`tool-${toolId}`}>
-            <a href={tool.url[lang]} target="_blank">
-              <img
-                src={getIconUrlForTheme(tool.logo, resolvedTheme)}
-                alt={tool.name}
-              />
-              <div className="popup">{tool.name}</div>
-            </a>
-          </li>
-        );
-      })}
-    </ul>
+    <div className="tools-list">
+      {isOverflowing && (
+        <>
+          <button
+            type="button"
+            className="left-button"
+            onClick={() => {
+              if (!ulRef.current) {
+                return;
+              }
+              ulRef.current.scroll({
+                left:
+                  ulRef.current.scrollLeft -
+                  ulRef.current.getBoundingClientRect().width / 2,
+                behavior: "smooth",
+              });
+            }}
+          >
+            <KeyboardArrowLeftIcon />
+          </button>
+          <button
+            type="button"
+            className="right-button"
+            onClick={() => {
+              if (!ulRef.current) {
+                return;
+              }
+              ulRef.current.scroll({
+                left:
+                  ulRef.current.scrollLeft +
+                  ulRef.current.getBoundingClientRect().width / 2,
+                behavior: "smooth",
+              });
+            }}
+          >
+            <KeyboardArrowRightIcon />
+          </button>
+        </>
+      )}
+      <ul ref={ulRef}>
+        {props.toolsIds.map((toolId: string) => {
+          if (props.isToolsLoading) {
+            return <div className="loader"></div>;
+          }
+          const tool: Tool | undefined = getTool(props.everyTools, toolId);
+          if (!tool) {
+            throw new Error(`tool ${toolId} not found`);
+          }
+          return (
+            <li key={`tool-${toolId}`}>
+              <a href={tool.url[lang]} target="_blank">
+                <img
+                  src={getIconUrlForTheme(tool.logo, resolvedTheme)}
+                  alt={tool.name}
+                />
+                <div className="popup">{tool.name}</div>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
